840ac238c1341926d47d0e36c9e42de7a6266c77
kavinsky-spy <juliorapa@hotmail.com>
2021-02-19 19:38:36 -0300

    Initial commit  Changes to be committed: 	new file:   .gitignore 	new file:   read.py 	new file:   rescale.py

---
+++ read.py
@@ -0,0 +1,22 @@
+import cv2 as cv;
+
+# img = cv.imread('Photos/ind23423ex.jpeg');
+#
+# cv.imshow('Julio', img)
+
+# Reading Videos
+# use int parameter for webcams connected.
+# capture = cv.VideoCapture('Videos/131733564_1490180371175304_5900651882737316230_n.mp4')
+#
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow('Video', frame)
+#
+#     if cv.waitKey(20) & 0xFF == ord('d'):
+#         break
+#
+# capture.release()
+# cv.destroyAllWindows()
+
+
+#cv.waitKey(0)
